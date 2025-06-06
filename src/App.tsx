685e16578950e3b1533d999263c5ffc70c1fd1a1
kvndlgs <poissedonn@gmail.com>
2025-06-06 09:40:48 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// import React from 'react';
-=======
 import React from 'react';
->>>>>>> 8dde1f7c81b1fb9df72cceb1a6e501e050d093c1
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { SubscriptionProvider } from './contexts/SubscriptionContext';
@@ -12,16 +8,12 @@ import HomePage from './pages/HomePage';
 import InterviewPage from './pages/InterviewPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-<<<<<<< HEAD
 import ProfilePage from './pages/ProfilePage';
 import PremiumPage from './pages/PremiumPage';
 import CustomCharactersPage from './pages/CustomCharactersPage';
 import DiscoverPage from './pages/DiscoverPage';
 
-=======
-import PremiumPage from './pages/PremiumPage';
-import CustomCharactersPage from './pages/CustomCharactersPage';
->>>>>>> 8dde1f7c81b1fb9df72cceb1a6e501e050d093c1
+
 
 function App() {
   return (
@@ -34,15 +26,12 @@ function App() {
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/interview" element={<InterviewPage />} />
-<<<<<<< HEAD
                 <Route path="/discover" element={<DiscoverPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path='/profile' element={<ProfilePage />} />
-=======
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
->>>>>>> 8dde1f7c81b1fb9df72cceb1a6e501e050d093c1
                 <Route path="/premium" element={<PremiumPage />} />
                 <Route path="/custom-characters" element={<CustomCharactersPage />} />
               </Routes>
